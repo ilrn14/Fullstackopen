19420af8cdd4ea2c7a72356829a534adb31719c4
ilrn14 <laurennguyen141@gmail.com>
2024-12-16 17:59:43 +0100

    1.6-1.10

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,56 +1,82 @@
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-  return (
-    <div>
-      <Header course={course} />
-      <Content course = {course}/>
-      <Total course={course} />
+import { use } from 'react'
+import { useEffect, useState } from 'react'
+
+const Button = (props) => {
+  return <button onClick={props.onClick}>{props.text}</button>
+}
+
+const StatisticLine = (props) => {
+    return (
+      <div>
+        {props.text} {props.value} 
       </div>
-  )
+    )
 }
 
-const Header = (props) => {
-  return (
-    <h1>{props.course.name}</h1>
-  )
+const Statistics = (props) => {
+   if (props.all === 0) {
+    return <div> No feedback given </div>
+   }
+
+    return (<div>
+      <StatisticLine text="good" value = {props.good}/>
+      <StatisticLine text="neutral" value = {props.neutral}/>
+      <StatisticLine text="bad" value = {props.bad}/>
+      <StatisticLine text="all" value = {props.all}/>
+      <StatisticLine text="average" value = {props.average}/>
+      <StatisticLine text="positive" value = {props.positive}/>
+    </div>)
 }
 
-const Content = (props) => {
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+   }
+
+   const handleBadClick = () => {
+    setBad(bad + 1)
+   }
+
+   useEffect(() => {
+      setAll(good + neutral + bad)
+     }, [good, neutral, bad])
+
+    useEffect(() => {
+      if(all > 0) {
+       setAverage((good - bad) / all)
+       setPositive((good / all) * 100)
+      }
+    }, [all])
+
   return (
     <div>
-      {props.course.parts[0].name} {props.course.parts[0].exercises}  
-      <br></br> 
-      {props.course.parts[1].name} {props.course.parts[1].exercises}
-      <br></br>
-      {props.course.parts[2].name} {props.course.parts[2].exercises}
+      <h1>give feedback</h1>
+      <Button onClick = {handleGoodClick} text="good"/>
+      <Button onClick={handleNeutralClick} text="neutral"/>
+      <Button onClick={handleBadClick} text="bad"/>
+      <h2> statistics </h2>
+      <Statistics
+         good = {good}
+         neutral = {neutral}
+         bad = {bad}
+         all = {all}
+         average = {average}
+         positive = {positive}
+      />
     </div>
   )
 }
-
-const Total= (props) => {
-  return (
-    <p>
-       Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}
-    </p>
-  )
-}
-
 
 
 export default App
