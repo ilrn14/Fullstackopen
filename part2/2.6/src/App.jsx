fe9f8b85f98ed68530082a075db037a62b0035da
ilrn14 <laurennguyen141@gmail.com>
2025-02-07 15:41:33 +0100

    2.7

--- part2/2.6/src/App.jsx
+++ part2/2.6/src/App.jsx
@@ -2,17 +2,17 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { id: 1, 
-      name: 'Arto Hellas' }
+    {  name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
-  const [nextId, setNextId] = useState(2);
   
   const addName = (event) => {
     event.preventDefault()
-    setPersons([...persons, { id: nextId, name: newName }]);
-    setNextId(nextId + 1);
-    console.log("persons", persons)
+    if (persons.some(person => person.name === newName)) {
+      window.alert(`"${newName}" is already added to phonebook`);
+    } else {
+      setPersons([...persons, { name: newName }]);
+    }
   }
 
   const handleNameChange = (event) => {
@@ -30,13 +30,13 @@ const App = () => {
                  onChange={handleNameChange}/>
         </div>
         <div>
-          <button type="submit"> add</button>
+          <button type="submit" > add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
         {persons.map((person) => (
-          <div key={person.id}>{person.name} </div>
+          <div key={person.name}>{person.name} </div>
         ))}
       </div>
     </div>
